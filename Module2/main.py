a73fae596afaa71b320975c3d5d61a34d27bf24a
FarhaN400 <fa07204377@gmail.com>
2026-09-13 13:30:43 +0530

    Update in Modeul2

--- Module2/main.py
+++ Module2/main.py
@@ -1,8 +1,5 @@
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
-from langchain_community.document_loaders import PyMuPDFLoader
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import JsonOutputParser
 from ocr import ocr_extract_pdf
 from prompt import notice_extraction_prompt , parser
 
@@ -14,8 +11,6 @@ llm = HuggingFaceEndpoint(
 )
 
 model = ChatHuggingFace(llm=llm)
-parser = JsonOutputParser()
-
 
 docs = ocr_extract_pdf('wbjee.pdf')
 
